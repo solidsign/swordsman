54dd02dd0345268419cec690df027289c6f798fc
solidsign <49648821+solidsign@users.noreply.github.com>
2021-10-28 02:05:45 +0300

    Added Dead state

---
+++ Assets/Scripts/States/Player/Dead.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Game.States.Player
+{
+    public class Dead : BaseState
+    {
+        private Animator _animator;
+        private static readonly int DeathAnimation = Animator.StringToHash(nameof(Dead));
+        public override void Init(StateHandler stateHandler)
+        {
+            _animator = stateHandler.GetComponent<Animator>();
+        }
+
+        public override void Enter()
+        {
+            _animator.SetTrigger(DeathAnimation);
+        }
+
+        public override string ToString()
+        {
+            return nameof(Dead);
+        }
+    }
+}
