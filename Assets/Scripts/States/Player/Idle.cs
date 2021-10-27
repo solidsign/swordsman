216d7a4ac23a6f979fe8622fe6109ef086640684
solidsign <49648821+solidsign@users.noreply.github.com>
2021-10-28 01:09:49 +0300

    Added Idle state

---
+++ Assets/Scripts/States/Player/Idle.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Game.States.Player
+{
+    public class Idle : BaseState
+    {
+        private Animator _animator;
+        private const string Name = "Idle";
+        private static readonly int IdleAnimation = Animator.StringToHash(Name);
+
+        public override void Init(StateHandler stateHandler)
+        {
+            _animator = stateHandler.GetComponent<Animator>();
+        }
+
+        public override void Enter()
+        {
+            _animator.SetTrigger(IdleAnimation);
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
