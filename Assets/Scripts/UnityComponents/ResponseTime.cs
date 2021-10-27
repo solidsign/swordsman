77fd8e6e5adeb93790a218750bdf0b68cba01d42
solidsign <49648821+solidsign@users.noreply.github.com>
2021-10-28 02:04:49 +0300

    Added ResponseTime.cs component

---
+++ Assets/Scripts/UnityComponents/ResponseTime.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public class ResponseTime : MonoBehaviour
+    {
+        [SerializeField] private float value;
+
+        public float Value => value;
+    }
+}
