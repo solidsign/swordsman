3a364dc3abd36e3508fa36f80521ee3bda8ae15d
solidsign <49648821+solidsign@users.noreply.github.com>
2021-10-28 02:06:08 +0300

    Added Attacked state

---
+++ Assets/Scripts/States/Player/Attacked.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Game.States.Player
+{
+    public abstract class Attacked : BaseState
+    {
+        private float _responseTime;
+        private StateHandler _stateHandler;
+        public override void Init(StateHandler stateHandler)
+        {
+            _stateHandler = stateHandler;
+            _responseTime = stateHandler.GetComponent<ResponseTime>().Value;
+        }
+
+        public override void Enter()
+        {
+            _stateHandler.StopAllCoroutines();
+            _stateHandler.StartCoroutine(WaitForResponse());
+        }
+
+        public override void Exit()
+        {
+            _stateHandler.StopAllCoroutines();
+        }
+
+        private IEnumerator WaitForResponse()
+        {
+            yield return new WaitForSeconds(_responseTime);
+            _stateHandler.SetState(nameof(Dead));
+        }
+    }
+}
