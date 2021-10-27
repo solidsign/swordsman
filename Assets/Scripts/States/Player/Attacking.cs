1f5ce6add6ba0bb27f75b3f8ce153bc7f7b03571
solidsign <49648821+solidsign@users.noreply.github.com>
2021-10-28 02:22:34 +0300

    Added Attacking state

---
+++ Assets/Scripts/States/Player/Attacking.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Game.States.Player
+{
+    public abstract class Attacking : BaseState
+    {
+        protected Animator _animator;
+        protected StateHandler _rival;
+        public override void Init(StateHandler stateHandler)
+        {
+            _animator = stateHandler.GetComponent<Animator>();
+            _rival = stateHandler.GetComponent<Rival>().RivalGameObject.GetComponent<StateHandler>();
+        }
+    }
+}
