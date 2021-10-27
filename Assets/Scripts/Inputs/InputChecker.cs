9d84aa3e4893ef769f71e5c0d11e91b2a1371016
solidsign <49648821+solidsign@users.noreply.github.com>
2021-10-28 00:52:51 +0300

    Added inputs to InputChecker.cs

--- Assets/Scripts/Inputs/InputChecker.cs
+++ Assets/Scripts/Inputs/InputChecker.cs
@@ -1,4 +1,6 @@
-﻿namespace Game.Inputs
+﻿using UnityEngine;
+
+namespace Game.Inputs
 {
     public class InputChecker
     {
@@ -9,5 +11,16 @@
             if (_instance == null) _instance = new InputChecker();
             return _instance;
         }
+
+        public bool MoveRight() => Input.GetKeyDown(KeyCode.RightArrow);
+        public bool MoveLeft() => Input.GetKeyDown(KeyCode.LeftArrow);
+        public bool UpperBlock() => Input.GetKeyDown(KeyCode.Keypad7);
+        public bool MiddleBlock() => Input.GetKeyDown(KeyCode.Keypad4);
+        public bool BottomBlock() => Input.GetKeyDown(KeyCode.Keypad1);
+        public bool UpperAttack() => Input.GetKeyDown(KeyCode.Keypad9);
+        public bool MiddleAttack() => Input.GetKeyDown(KeyCode.Keypad6);
+        public bool BottomAttack() => Input.GetKeyDown(KeyCode.Keypad3);
+        public bool Pause() => Input.GetKeyDown(KeyCode.Escape);
+        public bool Start() => Input.GetKeyDown(KeyCode.Space);
     }
 }
