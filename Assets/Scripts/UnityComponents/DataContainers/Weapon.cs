a211d015d1bfcc845afae90038198062a111e70c
solidsign <49648821+solidsign@users.noreply.github.com>
2021-10-28 22:26:10 +0300

    Added Gizmos

--- Assets/Scripts/UnityComponents/DataContainers/Weapon.cs
+++ Assets/Scripts/UnityComponents/DataContainers/Weapon.cs
@@ -6,8 +6,13 @@ namespace Game
     {
         [SerializeField] private float attackDistance;
         [SerializeField] private float prepareForAttackTime;
-
         public float AttackDistance => attackDistance;
         public float PrepareForAttackTime => prepareForAttackTime;
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, transform.position + attackDistance * transform.right);
+        }
     }
 }
