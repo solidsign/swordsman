22097085e04d119226473d41217a7a68187d5c3e
solidsign <49648821+solidsign@users.noreply.github.com>
2021-10-28 22:28:29 +0300

    Added VerifyNextState as blocking attack of appropriate direction

--- Assets/Scripts/States/Player/Blocking.cs
+++ Assets/Scripts/States/Player/Blocking.cs
@@ -1,13 +1,18 @@
-﻿using UnityEngine;
-
-namespace Game.States.Player
+﻿namespace Game.States.Player
 {
     public abstract class Blocking : BaseState
     {
+        private string _blockedAttack;
         protected AnimationSetter _animator;
         public override void Init(StateHandler stateHandler)
         {
+            _blockedAttack = nameof(Attacked) + this.ToString().Remove(0, nameof(Blocking).Length);
             _animator = stateHandler.GetComponent<AnimationSetter>();
+        }
+
+        public override bool VerifyNextState(string state)
+        {
+            return state != _blockedAttack;
         }
     }
 }
