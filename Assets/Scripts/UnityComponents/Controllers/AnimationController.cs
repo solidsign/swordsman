4b1c9ba61f8f121ec7cb1b60a70dcfac4a9526c9
solidsign <49648821+solidsign@users.noreply.github.com>
2021-10-28 19:43:05 +0300

    Created AnimationController.cs

---
+++ Assets/Scripts/UnityComponents/Controllers/AnimationController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Game.Core;
+
+namespace Game
+{
+    public class AnimationController : MonoBehaviour
+    {
+        private Animator _animator;
+        private Dictionary<AnimationName, string[]> _triggers;
+
+        [SerializeField] private List<string[]> _triggerNames;
+        
+        private void Start()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        public void SetAnimation(AnimationName animationName)
+        {
+            var animationVariants = _triggers[animationName];
+            int i = Random.Range(0, animationVariants.Length - 1);
+            _animator.SetTrigger(animationVariants[i]);
+        }
+        
+    }
+}
