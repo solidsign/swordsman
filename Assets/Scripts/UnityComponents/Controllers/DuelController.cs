223c825fc1b2b629009a83ea8880bbac3b8a6315
solidsign <49648821+solidsign@users.noreply.github.com>
2021-10-28 23:25:30 +0300

    Added Attack and CheckAttackDistance methods

--- Assets/Scripts/UnityComponents/Controllers/DuelController.cs
+++ Assets/Scripts/UnityComponents/Controllers/DuelController.cs
@@ -1,21 +1,37 @@
-﻿using UnityEngine;
+﻿using Game.Extra;
+using Game.States.Player;
+using UnityEngine;
 
 namespace Game
 {
     public class DuelController : MonoBehaviour
     {
-        [SerializeField] private Transform opponent1;
-        [SerializeField] private Transform opponent2;
+        [SerializeField] private StateHandler opponent1;
+        [SerializeField] private StateHandler opponent2;
 
+        private Transform t1;
+        private Transform t2;
         private void Awake()
         {
             opponent1.gameObject.AddComponent<DuelControllerInstance>().DuelController = this;
             opponent2.gameObject.AddComponent<DuelControllerInstance>().DuelController = this;
         }
 
-        public bool TryAttack(float attackDistance)
+        private void Start()
         {
-            return Vector3.Distance(opponent1.position, opponent2.position) <= attackDistance;
+            t1 = opponent1.transform;
+            t2 = opponent2.transform;
+        }
+
+        public bool CheckAttackDistance(float attackDistance)
+        {
+            return Vector3.Distance(t1.position, t2.position) <= attackDistance;
+        }
+
+        public void Attack(StateHandler attacker, Direction direction)
+        {
+            if(attacker == opponent1) opponent2.SetState(nameof(Attacked) + direction.ToString());
+            if(attacker == opponent2) opponent1.SetState(nameof(Attacked) + direction.ToString());
         }
     }
 }
