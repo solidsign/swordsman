07453a02b21496208edcc069fa5f8efae2b05da5
solidsign <49648821+solidsign@users.noreply.github.com>
2021-10-30 16:33:51 +0300

    Added interface for enabling/disabling duel ending effect

---
+++ Assets/Scripts/UnityComponents/Controllers/DuelEndEffect.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using Game.Extra;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+namespace Game
+{
+    public class DuelEndEffect : MonoBehaviour
+    {
+        [Range(0f, PlayerAnimationConfiguration.AttackStateTime)] [SerializeField] private float enableTime;
+        [SerializeField] private float disableTime;
+
+        private PostProcessVolume _volume;
+        private float _newWeight = 0f;
+
+        private void Awake()
+        {
+            _volume = GetComponent<PostProcessVolume>();
+        }
+
+        private IEnumerator UpdateWeight(float time)
+        {
+            if (Math.Abs(_newWeight - _volume.weight) < 0.05f) yield break;
+            float startValue = _volume.weight;
+            float t = 0f;
+            while (t < 1f)
+            {
+                _volume.weight = Mathf.Lerp(startValue, _newWeight, t);
+                t += Time.deltaTime / time;
+            }
+        }
+
+        public void EnableEffect()
+        {
+            StopAllCoroutines();
+            _newWeight = 1f;
+            StartCoroutine(UpdateWeight(enableTime));
+        }
+
+        public void DisableEffect()
+        {
+            StopAllCoroutines();
+            _newWeight = 0f;
+            StartCoroutine(UpdateWeight(disableTime));
+        }
+    }
+}
