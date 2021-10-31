2eefc148e219debf40cf1f4c7ccd35b383b4a5d5
solidsign <49648821+solidsign@users.noreply.github.com>
2021-10-31 19:51:12 +0300

    Set up triggers

--- Assets/Scripts/Inputs/AIInput.cs
+++ Assets/Scripts/Inputs/AIInput.cs
@@ -3,54 +3,137 @@
     public class AIInput : ISwordsmanInput // TODO: don't forget to implement AI
     {
         private DuelController _duel;
+        
+        private bool _startMoveRight = false;
+        private bool _startMoveLeft = false;
+        private bool _stopMove = false;
+        private bool _blockUp = false;
+        private bool _blockMiddle = false;
+        private bool _blockBottom = false;
+        private bool _attackUp = false;
+        private bool _attackMiddle = false;
+        private bool _attackBottom = false;
+        
+        
         public AIInput(DuelController duelController)
         {
             _duel = duelController;
         }
+
+        #region Triggers
+
+        public void TriggerStartMoveRight()
+        {
+            _startMoveRight = true;
+        }
+
+        public void TriggerStartMoveLeft()
+        {
+            _startMoveLeft = true;
+        }
+
+        public void TriggerStopMove()
+        {
+            _stopMove = true;
+        }
+
+        public void TriggerBlockUp()
+        {
+            _blockUp = true;
+        }
+
+        public void TriggerBlockMiddle()
+        {
+            _blockMiddle = true;
+        }
+
+        public void TriggerBlockBottom()
+        {
+            _blockBottom = true;
+        }
+
+        public void TriggerAttackUp()
+        {
+            _attackUp = true;
+        }
+
+        public void TriggerAttackMiddle()
+        {
+            _attackMiddle = true;
+        }
+
+        public void TriggerAttackBottom()
+        {
+            _attackBottom = true;
+        }
+
+
+        #endregion
+        #region Getters
         
         public bool StartMoveRight()
         {
-            return false;
+            if (!_startMoveRight) return false;
+            _startMoveRight = false;
+            return true;
         }
 
         public bool StartMoveLeft()
         {
-            return false;
+            if (!_startMoveLeft) return false;
+            _startMoveLeft = false;
+            return true;
         }
 
         public bool StopMove()
         {
-            return false;
+            if (!_stopMove) return false;
+            _stopMove = false;
+            return true;
         }
 
         public bool BlockUp()
         {
-            return false;
+            if (!_blockUp) return false;
+            _blockUp = false;
+            return true;
         }
 
         public bool BlockMiddle()
         {
-            return false;
+            if (!_blockMiddle) return false;
+            _blockMiddle = false;
+            return true;
         }
 
         public bool BlockBottom()
         {
-            return false;
+            if (!_blockBottom) return false;
+            _blockBottom = false;
+            return true;
         }
 
         public bool AttackUp()
         {
-            return false;
+            if (!_attackUp) return false;
+            _attackUp = false;
+            return true;
         }
 
         public bool AttackMiddle()
         {
-            return false;
+            if (!_attackMiddle) return false;
+            _attackMiddle = false;
+            return true;
         }
 
         public bool AttackBottom()
         {
-            return false;
+            if (!_attackBottom) return false;
+            _attackBottom = false;
+            return true;
         }
+        
+        #endregion
     }
 }
