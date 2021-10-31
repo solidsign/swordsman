bbf362562100bb465808da5e568e2de3b3b37189
solidsign <49648821+solidsign@users.noreply.github.com>
2021-10-31 20:05:31 +0300

    Made DuelController able to be managed externally

--- Assets/Scripts/UnityComponents/Controllers/DuelController.cs
+++ Assets/Scripts/UnityComponents/Controllers/DuelController.cs
@@ -7,28 +7,47 @@ namespace Game
 {
     public class DuelController : MonoBehaviour
     {
-        [SerializeField] private SwordsmanStateHandler player;
-        [SerializeField] private SwordsmanStateHandler ai;
+        [SerializeField] private Vector2 playerStartPosition;
+        [SerializeField] private Vector2 aiStartPosition;
+        
+        private SwordsmanStateHandler _player;
+        private SwordsmanStateHandler _ai;
 
         [SerializeField] private DuelEndEffect duelEndEffect;
 
         private SwordsmanStateHandler _defeated = null;
-        private void Awake()
+
+        public void Init(SwordsmanStateHandler player, SwordsmanStateHandler ai)
         {
-            player.gameObject.AddComponent<DuelControllerInstance>().DuelController = this;
-            ai.gameObject.AddComponent<DuelControllerInstance>().DuelController = this;
-            player.Init(PlayerInput.GetInstance());
-            ai.Init(new AIInput(this));
+            _player = player;
+            _ai = ai;
+
+            Instantiate(_player);
+            _player.transform.position = playerStartPosition;
+            Instantiate(_ai);
+            _ai.transform.position = aiStartPosition;
+            
+            _player.gameObject.AddComponent<DuelControllerInstance>().DuelController = this;
+            _ai.gameObject.AddComponent<DuelControllerInstance>().DuelController = this;
+            _player.Init(PlayerInput.GetInstance());
+            _ai.Init(new AIInput(this));
         }
 
-        
-        public string GetPlayerState() => player.GetCurrentState();
+        public void InstantiateNewAI(SwordsmanStateHandler ai)
+        {
+            _player.transform.position = playerStartPosition;
+            Instantiate(_ai);
+            _ai.transform.position = aiStartPosition;
+            _ai.gameObject.AddComponent<DuelControllerInstance>().DuelController = this;
+            _ai.Init(new AIInput(this));
+        }
+        public string GetPlayerState() => _player.GetCurrentState();
 
         public void Attack(SwordsmanStateHandler attacker, Direction direction, float attackDistance)
         {
             SwordsmanStateHandler attacked;
-            if (attacker == player) attacked = ai;
-            else if (attacker == ai) attacked = player;
+            if (attacker == _player) attacked = _ai;
+            else if (attacker == _ai) attacked = _player;
             else return;
 
             var distance = Vector2.Distance(attacker.Weapon.AttackPointPosition, attacked.BodyPosition.Value);
