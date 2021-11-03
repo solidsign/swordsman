fa5ba87c3c465f69a112036f414bd8fc17720ed7
solidsign <49648821+solidsign@users.noreply.github.com>
2021-11-03 21:08:39 +0300

    Added MoveFromPlayerDefencing.cs

---
+++ Assets/Scripts/States/AI/Substates/MoveFromPlayerDefencing.cs
@@ -0,0 +1,42 @@
+﻿using Game.Extra;
+using Game.Inputs;
+using Game.States.AI.Substates.Core;
+
+namespace Game.States.AI.Substates
+{
+    public class MoveFromPlayerDefencing : AISubstate
+    {
+        private AIInput _input;
+        private AIDuelLooker _looker;
+
+        public MoveFromPlayerDefencing(AIInput input, AIDuelLooker looker)
+        {
+            _input = input;
+            _looker = looker;
+        }
+        
+        public override void Enter()
+        {
+            if (!_looker.PlayerIsToTheLeft())
+            {
+                _input.TriggerStartMoveLeft();
+            }
+            else
+            {
+                _input.TriggerStartMoveRight();
+            }
+        }
+
+        public override void Exit()
+        {
+            _input.TriggerStopMove();
+        }
+
+        public override bool IsFinished => _looker.PlayerCanAttack() && _looker.PlayerIsAttacking();
+
+        public override string ToString()
+        {
+            return nameof(MoveFromPlayerDefencing);
+        }
+    }
+}
