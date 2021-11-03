cdb0e7b5bf8ae981b36516e804fc63965ad0a968
solidsign <49648821+solidsign@users.noreply.github.com>
2021-11-03 21:21:52 +0300

    Added Defencing.cs

---
+++ Assets/Scripts/States/AI/Defencing.cs
@@ -0,0 +1,23 @@
+﻿using Game.Extra;
+using Game.Inputs;
+using Game.States.AI.Core;
+using Game.States.AI.Substates;
+using Game.States.AI.Substates.Core;
+
+namespace Game.States.AI
+{
+    public class Defencing : AIState
+    {
+        public override void Init(AIInput input, AIDuelLooker looker)
+        {
+            _substates = new SubstatesChain()
+                .Add(new MoveFromPlayerDefencing(input, looker))
+                .Add(new Defence(input, looker));
+        }
+
+        public override string ToString()
+        {
+            return nameof(Defencing);
+        }
+    }
+}
