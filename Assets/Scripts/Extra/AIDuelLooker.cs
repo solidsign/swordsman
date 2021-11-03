49d8bff738e2109b92c8649e14f053d61f6df6f4
solidsign <49648821+solidsign@users.noreply.github.com>
2021-11-03 16:31:12 +0300

    Added check methods

--- Assets/Scripts/Extra/AIDuelLooker.cs
+++ Assets/Scripts/Extra/AIDuelLooker.cs
@@ -1,4 +1,6 @@
-﻿namespace Game.Extra
+﻿using UnityEngine;
+
+namespace Game.Extra
 {
     public class AIDuelLooker
     {
@@ -8,6 +10,30 @@
         {
             _player = player;
             _ai = ai;
+        }
+
+        public bool PlayerCanAttack()
+        {
+            var distance = Vector2.Distance(_player.Weapon.AttackPointPosition, _ai.BodyPosition.Value);
+            return distance <= _player.Weapon.AttackDistance;
+        }
+
+        public bool PlayerIsToTheLeft() => _player.BodyPosition.Value.x < _ai.BodyPosition.Value.x;
+
+        public bool AICanAttack()
+        {
+            var distance = Vector2.Distance(_ai.Weapon.AttackPointPosition, _player.BodyPosition.Value);
+            return distance <= _ai.Weapon.AttackDistance;
+        }
+
+        public bool PlayerIsAttacking() => _player.GetCurrentState().Contains(nameof(Game.States.Player.Attacking));
+
+        public Direction GetPlayersAttackDirection()
+        {
+            string state = _player.GetCurrentState();
+            if (state.Contains(Direction.Up.ToString())) return Direction.Up;
+            if (state.Contains(Direction.Middle.ToString())) return Direction.Middle;
+            return Direction.Bottom;
         }
     }
 }
