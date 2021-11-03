700ace3426bde383e1a317e8388c8cac019f26ca
solidsign <49648821+solidsign@users.noreply.github.com>
2021-11-03 16:38:04 +0300

    Added MoveToPlayerForAttack.cs

---
+++ Assets/Scripts/States/AI/Substates/MoveToPlayerForAttack.cs
@@ -0,0 +1,42 @@
+﻿using Game.Extra;
+using Game.Inputs;
+using Game.States.AI.Substates.Core;
+
+namespace Game.States.AI.Substates
+{
+    public class MoveToPlayerForAttack : AISubstate
+    {
+        private AIInput _input;
+        private AIDuelLooker _looker;
+
+        public MoveToPlayerForAttack(AIInput input, AIDuelLooker looker)
+        {
+            _input = input;
+            _looker = looker;
+        }
+
+        public override void Enter()
+        {
+            if (_looker.PlayerIsToTheLeft())
+            {
+                _input.TriggerStartMoveLeft();
+            }
+            else
+            {
+                _input.TriggerStartMoveRight();
+            }
+        }
+
+        public override void Exit()
+        {
+            _input.TriggerStopMove();
+        }
+
+        public override bool IsFinished => _looker.AICanAttack();
+        
+        public override string ToString()
+        {
+            return nameof(MoveToPlayerForAttack);
+        }
+    }
+}
