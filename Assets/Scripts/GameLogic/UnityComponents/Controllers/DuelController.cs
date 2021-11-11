3706f0e7e07d3129ff31855d68d7bb24827915eb
solidsign <49648821+solidsign@users.noreply.github.com>
2021-11-11 13:24:51 +0300

    Added Gizmos

--- Assets/Scripts/GameLogic/UnityComponents/Controllers/DuelController.cs
+++ Assets/Scripts/GameLogic/UnityComponents/Controllers/DuelController.cs
@@ -1,6 +1,5 @@
 ﻿using Game.Extra;
 using Game.Inputs;
-using UnityEditor;
 using UnityEngine;
 
 namespace Game
@@ -10,10 +9,10 @@ namespace Game
         [Header("Duelists")]
         [Header("Player")]
         [SerializeField] private SwordsmanStateHandler player;
-        [SerializeField] private Vector2 playerStartPosition;
+        [SerializeField] private float playerStartPositionX;
         [Header("AI")]
         [SerializeField] private SwordsmanStateHandler ai;
-        [SerializeField] private Vector2 aiStartPosition;
+        [SerializeField] private float aiStartPositionX;
         [Header("Post-Effects")]
         [SerializeField] private DuelEndEffect deathEffect;
         [Header("UI")]
@@ -45,12 +44,12 @@ namespace Game
 
         private void PlaceDuelists()
         {
-            if (playerStartPosition.x < aiStartPosition.x)
+            if (playerStartPositionX < aiStartPositionX)
                 ai.transform.eulerAngles = new Vector3(0, 180f, 0);
             else
                 player.transform.eulerAngles = new Vector3(0, 180f, 0);
-            player.transform.position = playerStartPosition;
-            ai.transform.position = aiStartPosition;
+            player.transform.position = new Vector3(playerStartPositionX, 0, 0);
+            ai.transform.position = new Vector3(aiStartPositionX,0,0);
         }
         
         private void HandleDuelEnd(SwordsmanStateHandler defeated)
@@ -63,6 +62,14 @@ namespace Game
                 else if (defeated == ai) winUI.Enable();
                 Destroy(defeated);
             });
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(new Vector3(playerStartPositionX,0,0), 0.5f);
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(new Vector3(aiStartPositionX,0,0), 0.5f);
         }
     }
 }
